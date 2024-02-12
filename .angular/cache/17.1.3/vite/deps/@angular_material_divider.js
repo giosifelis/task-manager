0d8d5306522e81fd40aa32995e514a7901dd611f
George Iosifelis <cubapiano@gmail.com>
2024-02-12 16:16:43 -0700

    material

---
+++ .angular/cache/17.1.3/vite/deps/@angular_material_divider.js
@@ -0,0 +1,13 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-6J7L2QJO.js";
+import "./chunk-4U2DESEZ.js";
+import "./chunk-FAJBWSHD.js";
+import "./chunk-YTO5THN3.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
+//# sourceMappingURL=@angular_material_divider.js.map
